a57f42058d2913fb40c45150b7b1a3ea12da8dd7
jwgameapp <jwgameapp@qq.com>
2012-06-12 22:38:09 +0800

    update HelloService class
    Signed-off-by: jwgameapp <jwgameapp@qq.com>

--- src/HelloService.java
+++ src/HelloService.java
@@ -4,6 +4,10 @@ public class HelloService
 	public void add(String name)
 	{
 		System.out.println("Hello "+name+" !!!!!");
+		System.out.println("Hello "+name+" !!!!!");
+		System.out.println("Hello "+name+" !!!!!");
+		System.out.println("Hello "+name+" !!!!!");
+		System.out.println("Hello "+name+" !!!!!");
 	}
 
 }
